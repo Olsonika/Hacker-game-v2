d236df767546644f07a16572e1522ad3aa3ba2d9
Olsonika <alek2257@easv365.dk>
2019-09-21 12:25:35 +0200

    Hacker game not working

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rock, paper</title>
+</head>
+<body>
+<form method="POST">
+<input type="submit" name="rock" value="Rock">
+<input type="submit" name="paper" value="Paper">
+<input type="submit" name="scissors" value="Scissors">
+    
+</body>
+</html>
+
+<?php
+
+$options = [ 
+    "paper",
+    "rock",
+    "scissors",
+];
+
+
+$_SESSION['pcchoice'] = $key = array_rand($options);
+echo $options[$key]; 
+
+if (isset($_POST["rock"]) && ($_SESSION['pcchoice'] == $options[0]))
+{
+    echo "You lost";
+};
+
+if (isset($_POST["rock"]) && ($_SESSION['pcchoice'] == $options[1])) 
+{
+    echo "Tie!";
+};
+
+if (isset($_POST["rock"]) && ($_SESSION['pcchoice'] == $options[2]))
+{
+    echo "You won!";
+};
